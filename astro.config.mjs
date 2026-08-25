9778441229f766c9b06bec92e9fda25c0e12e2a7
Robert Pito Sanchez <robsanchez124@gmail.com>
2026-08-25 19:46:07 -0400

    fixed headers

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://thejentosyproject.org',
+  site: 'https://jentosy.org',
   integrations: [sitemap()],
 });
